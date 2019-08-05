083b107159fa591338f71b084c0d1a5d2211b348
LSS <cjswosuny01@naver.com>
2019-08-05 17:02:31 +0900

    BFS 알고리즘 구현(Queue 활용)

--- JAVA_Algorithm/BFS/BFSTest.java
+++ JAVA_Algorithm/BFS/BFSTest.java
@@ -1,15 +1,5 @@
-﻿# BFS(Breadth First Search : 너비 우선 탐색)
+package com.ssafy.step2.bfs;
 
-트리에서 너비를 우선으로 탐색하는 알고리즘.
-
-**- Queue를 활용한 BFS 구현**
-
-① 처음 방문하는 정점을 방문처리 하고 Queue에 enQueue한다.  
-② Queue에 가자 앞에 있는 정점을 deQuque하고 해당 정점과 인접한 방문하지 않은 정점들을 모두 방문처리 한 후 Queue에 enQueue한다.  
-③ Queue가 모두 빌 때까지 ②를 반복한다.  
-④ ③에서 deQuque하여 나오는 순서가 BFS 순서이다.  
-
-```java
 import java.util.LinkedList;
 import java.util.Queue;
 
@@ -50,5 +40,3 @@ public class BFSTest {
 		}
 	}
 }
-
-```
