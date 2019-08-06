beb857e7d60704f5136ea4c68160fe3f7c0a1f07
LSS <cjswosuny01@naver.com>
2019-08-06 10:41:14 +0900

    분할정복에 퀵정렬 추가

--- JAVA_Algorithm/분할정복/QuickSort.java
+++ JAVA_Algorithm/분할정복/QuickSort.java
@@ -1,30 +1,5 @@
-﻿# 분할정복 알고리즘
+package com.ssafy.step1.divide;
 
-**① 분할(Divide)** : 해결할 ㅁ누제를 여러개의 작은 부분으로 나눔.  
-**② 정복(Conquer)** : 나눈 작은 문제를 해결.  
-**③ 통합(Combine)** : 해결된 답을 다시 통합.  
-ex) 거듭제곱, 퀵정렬
-
->거듭제곱 구현
-
-```java
-//거듭제곱 구현
-private static int power(int base, int exp) {
-	if (exp <= 0)
-		return 1;
-	int halfExp = exp / 2;
-	int result = power(base, halfExp);
-	result *= result;
-	if (exp % 2 == 1)
-		result *= base;
-	return result;
-}
-````
-
->퀵정렬 구현
-
-```java
-//퀵정렬
 import java.util.Arrays;
 
 public class QuickSort {
@@ -59,6 +34,3 @@ public class QuickSort {
 		}
 	}
 }
-
-}
-````
