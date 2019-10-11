df777ab7f452ec3f263c8db684a9c0114a4e25a5
LSS <cjswosuny01@naver.com>
2019-10-11 16:38:58 +0900

    sql 내용 추가

--- Database/참고/selectReview_join_subquery.sql
+++ Database/참고/selectReview_join_subquery.sql
@@ -1,17 +1,30 @@
 -- 업무별 급여 평균이 2000이상인 업무들을 조회
-
+select job, avg(sal) as avgSal
+from emp
+group by job
+having avgSal >= 2000;
 
 -- 부서별 급여 평균과, 전체 급여 평균을 조회하시오  
 -- 단 부서가 없는 사원은 프리랜서로 표시
-
+select if(grouping(deptno)=0, ifnull(deptno, '프리랜서'), '전체평균') as deptno, avg(sal) as avgSal
+from emp
+group by deptno with rollup;
 
 
 -- 사원번호, 이름, 급여, 부서번호, 부서명을 조회하시오
--- 단 부서에 배치되지 않는 사원도 조회하시오
+-- 단 부서에 배치되지 않은 사원도 조회하시오
+select empno, ename, sal, e.deptno, d.dname
+from emp as e left join dept as d
+on e.deptno = d.deptno;
 
 
 -- 게시글 3번에 대해 게시글번호, 작성자, 타이틀, 작성일(연-월-일), 내용
 -- 작성자가 올린파일 이름, 시스템에 저장된 파일 이름을 조회하시오
+select b.no, id, title, date_format(regdate, '%y-%m-%d') as date, contents
+from board b left join boardfile bf
+on b.no = bf.no
+where b.no = 3;
+
 
 /* SubQuery
    - Query문 내에 작성하는 Query를 SubQuery라고한다.
@@ -35,6 +48,10 @@
   - outer join한것과 같은 효과 
 */
  -- 사원번호, 이름, 급여, 부서번호, 부서명을 조회 
+select empno, ename, sal, e.deptno
+	,(select dname from dept where e.deptno = dept.deptno) as dname
+    ,(select loc from dept where e.deptno = dept.deptno) as loc
+from emp e;
 
  
  /*
@@ -55,20 +72,27 @@
  
  -- 단일 행 서브쿼리 
  -- 사원번호가 7369인 사원의 업무와 같은 업무를 하는 사원의 모든 조회
- 
+ select *
+ from emp
+ where job = (select job from emp where empno = 7369);
  
  select * from emp where empno=7369;
  
+ 
  -- 평균 급여보다 많이 받는 사원의 모든 정보를 조회
- 
- 
+ select *
+ from emp
+ where sal > (select avg(sal) from emp);
+  
 select avg(sal) from emp;
+
 
 -- 업무가 'MANAGER'인 사원들 중 전체 사원의 평균 급여보다 많이 받는 사원들에 대해
 -- 사원번호, 업무, 급여, 급여 등급을 조회
-
-
-
+select empno, job, sal, grade
+from emp join salgrade
+on sal between losal and hisal
+where job = 'MANAGER' and sal > (select avg(sal) from emp);
 
 
 
@@ -90,15 +114,22 @@ select avg(sal) from emp;
 
 -- in  
 -- 부서 번호 10번에 근무하는 사원들과 같은 급여를 받는 사원들을 조회하세요
-
+select *
+from emp
+where sal in (select sal from emp where deptno = 10);
 
 
 -- 다음 관리자들이 관리하는 사원들의 모든 정보를 조회.
 -- SCOTT, CLARK 
-
+select *
+from emp
+where mgr in (select empno from emp where ename in ('SCOTT','CLARK'));
 
 
 -- 각 부서의 평균 급여들 보다 많이 받는 사원의 정보를 조회
+select *
+from emp
+where sal > all(select avg(sal) from emp group by deptno);
 
 select * from emp;
 
@@ -115,7 +146,9 @@ select avg(sal) from emp group by job;
  형식]  where  (컬럼, 컬럼,..) 비교 조건(select 컬럼, 컬럼,.. from ~)
 */
 -- ADAMS 사원의 업무와 부서가 같은 사원의 정보를 조회하시오
-
+select *
+from emp
+where (job, deptno) = (select job, deptno from emp where ename = 'ADAMS');
 
 
 /*
@@ -125,15 +158,20 @@ select avg(sal) from emp group by job;
 */
 
 -- 사원이 소속된 부서의 평균 급여보다 많이 받는 사원의 정보를 조회
-
-
+select *
+from emp e
+where sal > (select avg(sal) from emp e2 where e.deptno = e2.deptno);
+-- 아래도 같은 결과(from 절의 sub query로 변경)
+select *
+from emp e, (select avg(sal) as avgSal, deptno as avgdeptno from emp group by deptno) d
+where sal > avgSal and deptno = avgdeptno;
 
 select deptno, avg(sal)
-from   emp
+from emp
 group by deptno;
 
--- 부서별 최저 임금을 받는 사원의 모든 정보를 조회 
 
+-- 부서별 최저 임금을 받는 사원의 모든 정보를 조회 
 
 
 
@@ -147,12 +185,15 @@ group by deptno;
 */
 
 -- 부하직원이 있는 사원에 대한 정보 추출 
-
-
+select *
+from emp e
+where exists (select 1 from emp e2 where e.empno = e2.mgr);
 
 
 -- 판매된적이 있는 상품의 정보를 조회하시오                 
-
+select *
+from goods g
+where exists (select 1 from orders o where g.gno = o.gno);
 
 /*
 	not exists 
@@ -162,8 +203,7 @@ group by deptno;
 -- 한번도 판매 되지 않은 상품을 조회 
 select *
 from   goods g
-where  not exists  (select 1 from orders o
-					where  g.gno = o.gno);
+where  not exists  (select 1 from orders o where g.gno = o.gno);
 
 
 
@@ -214,7 +254,10 @@ where  sal <= all (select sal from emp d
 
 -- 급여가 높은 사원 5명 조회 
 -- mysql 
-
+select *
+from emp
+order by sal desc
+limit 0, 5;
               
 -- oralce , db, ms-sql             
 select  rownum, a.*
@@ -225,7 +268,7 @@ where   rownum <=5;
 
 -- 급여가 높은 사원 6~10위까지 
 -- mysql 
-   
+
 
 -- oralce , db, ms-sql   
 select *          
@@ -236,25 +279,52 @@ from  ( select  rownum ro, a.*
 	   ) 
 where   ro between 6 and 10;
 
-
-
 -- 이번달에 판매한 상품의 번호, 수량을  많이 판매한 순으로 조회 하시오
+select g.gno, sum(quantity) as sumQuantity
+from goods g join orders o
+on g.gno = o.gno
+where date_format(o.odate, '%y-%m') = date_format(current_date(), '%y-%m')
+order by sumQuantity desc;
 
+-- 판매량이 가장 많은 상품 2개를 조회하여 상품 번호, 상품명, 가격, 총 판매된 수량, 순위를 표시하시오.
+select rank() over (order by sum(quantity) desc) as 순위, g.gno, brand, price, sum(quantity) as sumQuantity
+from goods g join orders o
+on g.gno = o.gno
+group by g.gno
+order by sumQuantity desc
+limit 0, 2;
 
--- 판매량이 가장 많은 상품 2개를 조회하여 상품 번호, 상품명
--- , 가격, 총 판매된 수량, 순위를 표시하시오.
+select gno, brand, price, amount
+from goods
+join (select gno, sum(quantity) as amount from orders group by gno limit 0, 2) a
+using (gno)
+order by amount desc;
 
-
--- 이번달에 판매량이 가장 많은 상품 2개를 조회하여 상품 번호, 상품명
--- , 가격, 총 판매된 수량, 순위를 표시하시오.
-
+-- 이번달에 판매량이 가장 많은 상품 2개를 조회하여 상품 번호, 상품명, 가격, 총 판매된 수량, 순위를 표시하시오.
+select rank() over (order by sum(quantity) desc) as 순위, g.gno, brand, price, sum(quantity) as sumQuantity
+from goods g join orders o
+on g.gno = o.gno
+where date_format(o.odate, '%y-%m') = date_format(current_date(), '%y-%m')
+group by g.gno
+order by sumQuantity desc
+limit 0, 2;
 
 -- 제품의 판매 금액이 높은 상품 3개를 조회 하시오 
-
+select gno, brand, amount, price, amount * price as totalSalPrice
+from goods
+join (select gno, sum(quantity) as amount from orders group by gno) o
+using (gno)
+order by totalSalPrice desc
+limit 0, 3;
 
 -- category(분류별) 판매 수량 구하기 
--- 분류코드(cno), 분류명(name), 판매수량, 판매 금액을
--- 판매 수량이 높은 순으로 조회하세요      
+-- 분류코드(cno), 분류명(name), 판매수량, 판매 금액을 판매 수량이 높은 순으로 조회하세요      
+select cno, name, ifnull(amount, 0) as amount, ifnull(totalPrice, 0) as totalPrice
+from category
+left join (select cno, sum(quantity) as amount, sum(total) as totalPrice
+from (select gno, cno, quantity, quantity * price as total from orders join goods using (gno)) a group by cno) o
+using (cno)
+order by amount desc;
 
 -- 상품을 기준으로 올해 판매한 상품들을 월별 Pivot테이블 형태로 조회 하시오 
 
