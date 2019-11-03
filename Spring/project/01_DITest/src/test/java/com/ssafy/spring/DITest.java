9ff1010732ca4ce61a4245f1ca67282053853e1b
LSS <cjswosuny01@naver.com>
2019-11-04 08:20:26 +0900

    getBeans -> getBean 메서드 명 변경

--- Spring/project/01_DITest/src/test/java/com/ssafy/spring/DITest.java
+++ Spring/project/01_DITest/src/test/java/com/ssafy/spring/DITest.java
@@ -15,11 +15,11 @@ public class DITest {
 		System.out.println("스프링 콘테이너 로딩 완료!");
 
 		/**
-		 * getBeans(String name)
+		 * getBean(String name)
 		 *  - name 이나 id에 해당하는 객체를 container에서 추출
 		 *  - object로 반환하므로 형변환을 해야 한다.
 		 *  
-		 * getBeans(Class)
+		 * getBean(Class)
 		 *  - 지정한 클래스 타입과 동일한 객체를 container에서 추출
 		 *  - 지정한 클래스 타입으로 반환하므로 형변환이 필요하지 않다.
 		 *  - 같은 타입의 객체가 2개 이상 등록되어 있으면 에러가 나는 것에 주의한다.
